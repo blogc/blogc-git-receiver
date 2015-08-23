57b0f1a449353d4b02bc7800b6f40daf61d3ebbb
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2015-08-23 01:43:23 -0300

    now it is able to build sites properly

--- blogc-git-receiver.c
+++ blogc-git-receiver.c
@@ -20,13 +20,15 @@
 #include <errno.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <dirent.h>
+#include <time.h>
 
 #ifndef BUFFER_SIZE
 #define BUFFER_SIZE 1024
 #endif
 
 
-char*
+static char*
 b_strdup_vprintf(const char *format, va_list ap)
 {
     va_list ap2;
@@ -47,7 +49,7 @@ b_strdup_vprintf(const char *format, va_list ap)
 }
 
 
-char*
+static char*
 b_strdup_printf(const char *format, ...)
 {
     va_list ap;
@@ -55,6 +57,60 @@ b_strdup_printf(const char *format, ...)
     char *tmp = b_strdup_vprintf(format, ap);
     va_end(ap);
     return tmp;
+}
+
+
+static void
+rmdir_recursive(const char *dir)
+{
+    struct stat buf;
+    if (0 != stat(dir, &buf)) {
+        fprintf(stderr, "error: failed to remove directory (%s): %s\n", dir,
+            strerror(errno));
+        exit(2);
+    }
+    if (!S_ISDIR(buf.st_mode)) {
+        fprintf(stderr, "error: trying to remove invalid directory: %s\n", dir);
+        exit(2);
+    }
+    DIR *d = opendir(dir);
+    if (d == NULL) {
+        fprintf(stderr, "error: failed to open directory: %s\n",
+            strerror(errno));
+        exit(2);
+    }
+    struct dirent *e;
+    while (NULL != (e = readdir(d))) {
+        if ((0 == strcmp(e->d_name, ".")) || (0 == strcmp(e->d_name, "..")))
+            continue;
+        char *f = b_strdup_printf("%s/%s", dir, e->d_name);
+        if (0 != stat(f, &buf)) {
+            fprintf(stderr, "error: failed to stat directory entry (%s): %s\n",
+                e->d_name, strerror(errno));
+            free(f);
+            exit(2);
+        }
+        if (S_ISDIR(buf.st_mode)) {
+            rmdir_recursive(f);
+        }
+        else if (0 != unlink(f)) {
+            fprintf(stderr, "error: failed to remove file (%s): %s\n", f,
+                strerror(errno));
+            free(f);
+            exit(2);
+        }
+        free(f);
+    }
+    if (0 != closedir(d)) {
+        fprintf(stderr, "error: failed to close directory: %s\n",
+            strerror(errno));
+        exit(2);
+    }
+    if (0 != rmdir(dir)) {
+        fprintf(stderr, "error: failed to remove directory: %s\n",
+            strerror(errno));
+        exit(2);
+    }
 }
 
 
@@ -295,6 +351,8 @@ git_hook(int argc, char *argv[])
     }
     free(git_archive_cmd);
 
+    char *repo_dir = get_current_dir_name();
+
     if (0 != chdir(dir)) {
         fprintf(stderr, "error: failed to chdir (%s): %s\n", dir,
             strerror(errno));
@@ -307,28 +365,67 @@ git_hook(int argc, char *argv[])
         goto cleanup;
     }
 
-    char *gmake_cmd = b_strdup_printf(GMAKE_BINARY " OUTPUT_DIR=_buildroot");
+    char *home = getenv("HOME");
+    if (home == NULL) {
+        fprintf(stderr, "error: failed to find user home path\n");
+        rv = 1;
+        goto cleanup;
+    }
+
+    unsigned long epoch = time(NULL);
+    char *output_dir = b_strdup_printf("%s/builds/%s-%lu", home, new, epoch);
+    char *gmake_cmd = b_strdup_printf(GMAKE_BINARY " OUTPUT_DIR=\"%s\"",
+        output_dir);
     if (0 != system(gmake_cmd)) {
         fprintf(stderr, "error: failed to build website ...\n");
         rv = 1;
+        free(output_dir);
+        free(gmake_cmd);
         goto cleanup;
     }
     free(gmake_cmd);
 
-    fprintf(stderr, "%s\n", new);
+    if (0 != chdir(repo_dir)) {
+        fprintf(stderr, "error: failed to chdir (%s): %s\n", repo_dir,
+            strerror(errno));
+        rv = 1;
+        rmdir_recursive(output_dir);
+        free(output_dir);
+        goto cleanup;
+    }
 
+    char *htdocs_sym = NULL;
+    ssize_t htdocs_sym_len = readlink("htdocs", buffer, BUFFER_SIZE);
+    if (0 < htdocs_sym_len) {
+        if (0 != unlink("htdocs")) {
+            fprintf(stderr, "error: failed to remove symlink (%s/htdocs): %s\n",
+                repo_dir, strerror(errno));
+            rv = 1;
+            rmdir_recursive(output_dir);
+            free(output_dir);
+            goto cleanup;
+        }
+        buffer[htdocs_sym_len] = '\0';
+        htdocs_sym = buffer;
+    }
+
+    if (0 != symlink(output_dir, "htdocs")) {
+        fprintf(stderr, "error: failed to create symlink (%s/htdocs): %s\n",
+            repo_dir, strerror(errno));
+        rv = 1;
+        rmdir_recursive(output_dir);
+        free(output_dir);
+        goto cleanup;
+    }
+
+    if (htdocs_sym != NULL)
+        rmdir_recursive(htdocs_sym);
 
 cleanup:
-    rm_cmd = b_strdup_printf("rm -rf \"%s\"", dir);
-    if (0 != system(rm_cmd)) {
-        fprintf(stderr, "error: failed to remove temporary directory: %s\n",
-            dir);
-        free(rm_cmd);
-        return 0;
-    }
-    free(rm_cmd);
+    rmdir_recursive(dir);
 cleanup2:
     free(new);
+    free(repo_dir);
     return rv;
 }
 
